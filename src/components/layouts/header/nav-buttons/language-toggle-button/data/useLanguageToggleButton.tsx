e3268c7e10547a6a8c6278c3b41e0af688f45db4
devHong <choisieun89@gmail.com>
2023-07-12 20:32:46 +0900

    Add rtk & lang store

---
+++ src/components/layouts/header/nav-buttons/language-toggle-button/data/useLanguageToggleButton.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "src/store";
+import { commonSlice } from ".";
+
+const useLanguageToggleButton = () => {
+  const selectedLang = useAppSelector((state) => state.common.language);
+  const dispatch = useAppDispatch();
+
+  const langList = [
+    {
+      lable: "KO",
+      value: "ko",
+    },
+    {
+      lable: "EN",
+      value: "en",
+    },
+    {
+      lable: "CN",
+      value: "cn",
+    },
+  ];
+  const [isShowLangList, setIsShowLangList] = useState(false);
+
+  type Language = "ko" | "en";
+
+  const handleSetSelectedLang = (lang: Language) => {
+    dispatch(commonSlice.actions.setLanguage(lang));
+  };
+
+  return {
+    selectedLang,
+    dispatch,
+    langList,
+    isShowLangList,
+    setIsShowLangList,
+    handleSetSelectedLang,
+  };
+};
+
+export default useLanguageToggleButton;
