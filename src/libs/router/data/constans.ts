6eb6814db55f281347aa240be112995a2d378e9a
devHong <choisieun89@gmail.com>
2023-07-12 14:16:38 +0900

    change route proxy

--- src/libs/router/data/constans.ts
+++ src/libs/router/data/constans.ts
@@ -8,13 +8,22 @@ const routers = {
   LOGIN: {
     path: "/login",
     description: "Login Page",
+
+    FIND_PWD: {
+      path: "/find-pw",
+      description: "find password",
+
+      FIND_PWD11: {
+        path: "/find-pw11",
+        description: "find password",
+      },
+    },
   },
 };
 
-// FIXME: any type change
-const setNestedPathProxy: ProxyHandler<any> = {
-  get(target: RouteItem, key: keyof typeof routers): RouteItem | string {
-    const value = target[key];
+const setNestedPathProxy: ProxyHandler<RouteItem> = {
+  get(target: RouteItem, p: keyof typeof routers): RouteItem | string {
+    const value = target[p];
     if (typeof value === "object" && value !== null) {
       const parentPath = target.path ? target?.path : "";
       return new Proxy(
@@ -22,10 +31,10 @@ const setNestedPathProxy: ProxyHandler<any> = {
         setNestedPathProxy
       );
     }
-    return target[key];
+    return `${import.meta.env.VITE_SERVER_DOMAIN}${value}`;
   },
 };
-
+// FIXME:
+// @ts-ignore
 const ROUTES = new Proxy(routers, setNestedPathProxy);
-
 export default ROUTES;
